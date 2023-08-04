9848bb0cd5308fa194c58a405465f14f71c6b3d5
Young-Gaius <youngehu8@gmail.com>
2023-08-04 13:53:05 +0100

    add content to js filr

--- src/modules/globalvar.js
+++
@@ -1,8 +0,0 @@
-const listContainer = document.querySelector('.list');
-const inputName = document.querySelector('#name');
-const inputScore = document.querySelector('#score');
-const formSubmit = document.querySelector('.formItem');
-
-export {
-  listContainer, formSubmit, inputName, inputScore,
-};
